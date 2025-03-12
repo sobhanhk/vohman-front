9086f74b3aff8276bdc1bd48e3b870bb2dc1dba7
sobhanhk <sobhan.hk@outlook.com>
2025-03-12 16:26:56 +0330

    start of Project?

--- script/script.js
+++ script/script.js
@@ -2,26 +2,43 @@ const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
-  slidesPerView: 4,
+  slidesPerView: 5,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
 
-  // // If we need pagination
-  // pagination: {
-  //   el: ".swiper-pagination",
-  // },
+    // // If we need pagination
+    // pagination: {
+    //   el: ".swiper-pagination",
+    // },
 
-  // // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    // // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    freeMode: {
+      enable: true,
+    },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: ".swiper-scrollbar",
+    //   draggable: true,
+    // },
   },
-
-  freeMode: {
-    enable: true,
-  },
-
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: ".swiper-scrollbar",
-  //   draggable: true,
-  // },
 });
